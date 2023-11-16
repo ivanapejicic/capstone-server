40004137bbf9d8dfe53a95b14afb3f2b1640033a
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-15 22:52:09 -0500

    Folder structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
